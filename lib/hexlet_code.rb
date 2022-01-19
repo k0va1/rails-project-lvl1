9a1a5875859f97bb91b12f3b02d8f8515d149009
k0va1 <al3xander.koval@gmail.com>
2022-01-19 21:53:06 +0300

    add root path to gem

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,8 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.root
+    Pathname.new(File.expand_path('../..', __FILE__))
+  end
 end
