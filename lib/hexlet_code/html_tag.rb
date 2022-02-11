1eca36d909e378ccc5d48333175191921cdc8dd0
k0va1 <al3xander.koval@gmail.com>
2022-02-11 19:37:18 +0300

    remove unnecessary docs

--- lib/hexlet_code/html_tag.rb
+++ lib/hexlet_code/html_tag.rb
@@ -3,25 +3,6 @@
 require_relative 'tag'
 
 module HexletCode
-  # How to use?
-  # HexletCode::HtmlTag.build('br')
-  # <br>
-
-  # HexletCode::HtmlTag.build('img', src: 'path/to/image')
-  # <img src="path/to/image">
-
-  # HexletCode::HtmlTag.build('input', type: 'submit', value: 'Save')
-  # <input type="submit" value="Save">
-
-  # Для парных тегов тело передается как блок
-  # HexletCode::HtmlTag.build('label') { 'Email' }
-  # <label>Email</label>
-
-  # HexletCode::HtmlTag.build('label', for: 'email') { 'Email' }
-  # <label for="email">Email</label>
-
-  # HexletCode::HtmlTag.build('div')
-  # <div></div>
   class HtmlTag < ::HexletCode::Tag
     class << self
       def build(tag_name, params = {})
